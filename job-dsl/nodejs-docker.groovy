96b9298d182eef294523f13112b59e093f385971
Sachithra Manamperi <sachithram@zone24x7.lk>
2023-01-28 14:20:44 +0530

    Commit2

---
+++ job-dsl/nodejs-docker.groovy
@@ -0,0 +1,23 @@
+job('NodeJS Docker example') {
+    scm {
+        git('https://github.com/SachPrecious/job-dsl.git') 
+    }
+    triggers {
+        scm('H/5 * * * *')
+    }
+    wrappers {
+        nodejs('nodejs') // this is the name of the NodeJS installation in 
+                         // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
+    }
+    steps {
+        dockerBuildAndPublish {
+            repositoryName('sachithram/nodejs-demo')
+            tag('${GIT_REVISION,length=9}')
+            registryCredentials('dockerhub')
+            forcePull(false)
+            forceTag(false)
+            createFingerprints(false)
+            skipDecorate()
+        }
+    }
+}
